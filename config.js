1b117a8b77e1483c09b61dfb7c3e9f5e3bb105b5
vltruong <111220221+vltruong@users.noreply.github.com>
2022-09-03 23:58:16 +0700

    Add files via upload
    creat sn

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Trần Thị Yến';
+const giftUrl = 'https://happybdtranyen.glitch.me';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Trần Yến';
+const contentCard = 'Chúc mừng sinh nhật cậu tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'MyAvatar.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
